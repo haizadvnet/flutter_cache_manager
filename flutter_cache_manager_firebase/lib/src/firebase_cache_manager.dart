eac58589986ade4e550ee270be42a1ed71f324c1
haizadvnet <haizad@vn.my>
2024-01-09 09:29:07 +0800

    - dynamic cache key

--- flutter_cache_manager_firebase/lib/src/firebase_cache_manager.dart
+++ flutter_cache_manager_firebase/lib/src/firebase_cache_manager.dart
@@ -6,15 +6,14 @@ import 'firebase_http_file_service.dart';
 /// Use [FirebaseCacheManager] if you want to download files from firebase storage
 /// and store them in your local cache.
 class FirebaseCacheManager extends CacheManager with ImageCacheManager {
-  late final String key;
+  static const key = 'firebaseCache';
 
-  static late final FirebaseCacheManager _instance;// = FirebaseCacheManager._();
+  static late final FirebaseCacheManager _instance = FirebaseCacheManager._();
 
-  factory FirebaseCacheManager(String key) {
-    _instance = FirebaseCacheManager._(key);
+  factory FirebaseCacheManager() {
     return _instance;
   }
 
-  FirebaseCacheManager._(this.key)
+  FirebaseCacheManager._()
       : super(Config(key,fileSystem: IOFileSystem(key),fileService: FirebaseHttpFileService()));
 }
